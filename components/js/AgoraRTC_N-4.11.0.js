9547cee9d7f7e53f6b9a6bd77c9dbf5bfd063b97
=AmanShivdasani <=AmanShivdasani2004@gmail.com>
2024-10-20 10:58:37 +0530

    improvement

--- components/js/AgoraRTC_N-4.11.0.js
+++ components/js/AgoraRTC_N-4.11.0.js
@@ -1,24 +1,4 @@
-/*
- * Green Coders Hackathon Project 
- * C.V. Raman Global University, Bhubaneswar, Odisha, India
- * 
- * Copyright 2024 Sudhanshu Kumar 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * AgoraWebSDK_N-v4.11.0-0-g961abf54 Copyright AgoraInc.
- */
+
 
 ! function(e, t) { "object" == typeof exports && "undefined" != typeof module ? module.exports = t() : "function" == typeof define && define.amd ? define(t) : (e = "undefined" != typeof globalThis ? globalThis : e || self).AgoraRTC = t() }(this, (function() {
     "use strict";
--- components/js/agora-rtm-sdk-1.4.4.js
+++ components/js/agora-rtm-sdk-1.4.4.js
@@ -1,29 +1,4 @@
-/*
- * Green Coders Hackathon Project 
- * C.V. Raman Global University, Bhubaneswar, Odisha, India
- * 
- * Copyright 2024 Sudhanshu Kumar 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-/*
- @preserve
- AgoraRTM Web SDK 1.4.4 - commit: v1.4.4-0-g3d73f979
- Copyright (C) 2018-2021 Agora Lab.
- This file is licensed under the AGORA, INC. SDK LICENSE AGREEMENT
- A copy of this license may be found at https://www.agora.io/en/sdk-license-agreement/
-*/
 "use strict";
 ! function(e, t) { "object" == typeof exports && "undefined" != typeof module ? module.exports = t() : "function" == typeof define && define.amd ? define(t) : (e = "undefined" != typeof globalThis ? globalThis : e || self).AgoraRTM = t() }(this, (function() {
     function de(e, t) {
